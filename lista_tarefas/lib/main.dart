53b96b13ce4428df005243e296a7b248cd998415
samuel <samuelshingen@hotmail.com>
2020-11-06 00:25:10 -0300

    Implementada a ordenação da lista, exclusão das tarefas finalizadas e a Snackbar

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -17,6 +17,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String,dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
 
   @override
@@ -38,6 +40,19 @@ class _HomeState extends State<Home> {
        _toDoList.add(newToDo);
        _saveData();
      });
+  }
+
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+    _toDoList.sort((a,b){
+    if (a["ok"] && !b ["ok"]) return 1;
+    else if (!a["ok"] && b["ok"]) return 0;
+    else return 0;
+  });
+  _saveData();
+});
+   return null;
   }
 
   @override
@@ -73,30 +88,83 @@ class _HomeState extends State<Home> {
              ),
            ),
           Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
+              child: RefreshIndicator(onRefresh: _refresh,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: (context, index){
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"]?
-                      Icons.check: Icons.error),),
-                    onChanged: (c){
-                      setState(() {
-                        _toDoList[index]["ok"]=c;
-                        _saveData();
-
-                      });
-                    },
-                  );
-            }),
+                  itemBuilder: buildItem),),
           )
         ],
       ),
     );
   }
+  Widget buildItem (BuildContext context, int index){
+    return Dismissible(
+      key: Key(DateTime.now().millisecond.toString()),
+      background: Container(
+       color: Colors.blueAccent,
+        child: Align(
+          alignment: Alignment(-0.9,0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"]?
+          Icons.check: Icons.error),),
+        onChanged: (c){
+          setState(() {
+            _toDoList[index]["ok"]=c;
+            _saveData();
+
+          });
+        },
+      ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+          final snack = SnackBar(
+            content: Text("Tarefa\"${_lastRemoved["title"]}\"removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: (){
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+
+                });
+                },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+/*  CheckboxListTile(
+  title: Text(_toDoList[index]["title"]),
+  value: _toDoList[index]["ok"],
+  secondary: CircleAvatar(
+  child: Icon(_toDoList[index]["ok"]?
+  Icons.check: Icons.error),),
+  onChanged: (c){
+  setState(() {
+  _toDoList[index]["ok"]=c;
+  _saveData();
+
+  });
+  },
+  );*/
+
   Future<File> _getFile() async{
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
